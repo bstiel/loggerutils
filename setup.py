ab8b32c3aba1114ec525e721478f7573108d5da0
Bjoern Stiel <bjoern.stiel@zoomeranalytics.com>
2018-11-30 06:40:09 +0000

    Added travis.yml

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
-setup(name='funniest',
-      version='0.1',
+setup(name='loggingutils',
+      version='0.1.0',
       description='Celery optimised log formatter',
       url='http://github.com/bstiel/loggingutils',
       author='Bjoern Stiel',
       author_email='bjoern.stiel@distributedpython.com',
       license='BSD-3-Clause',
       packages=['loggingutils'],
+      python_requires=">=3.0",
       test_suite='nose.collector',
       tests_require=['nose'],
+      install_requires=[
+            'Jinja2'
+      ],
       zip_safe=False)
